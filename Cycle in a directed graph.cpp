0549a3e0b72d4abab17aeadffc27448604c14545
codeking456 <sm544735@gmail.com>
2018-06-27 23:14:25 +0530

    Create Cycle in a directed graph.cpp

---
+++ Cycle in a directed graph.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+void topologicalsort(vector<ll> v1[100], ll v)
+{
+	queue<ll> q;
+	vector<ll> indegree(100);
+	for (ll i = 1; i <= v; i++)
+	{
+		for (auto it = v1[i].begin(); it != v1[i].end(); it++)
+			indegree[*it]++;
+	}
+	for (ll i = 1; i <= v; i++)
+	{
+		if (indegree[i] == 0)
+			q.push(i);
+	}
+	ll c = 0;
+	while (!q.empty())
+	{
+		ll f = q.front();
+		q.pop();
+		for (auto it = v1[f].begin(); it != v1[f].end(); it++)
+		{
+			indegree[*it] = indegree[*it] - 1;
+			if (indegree[*it] == 0)
+				q.push(*it);
+		}
+		c++;
+	}
+	if (c != v)
+	{
+		cout << "cycle";
+		return;
+	}
+  else
+  cout<<"no cycle";
+}
+int main()
+{
+	vector<ll> v1[100];
+	ll v, e, a, b;
+	cin >> v >> e;
+	for (ll i = 0; i < e; i++)
+	{
+		cin >> a >> b;
+		v1[a].push_back(b);
+	}
+	topologicalsort(v1, v);
+}
