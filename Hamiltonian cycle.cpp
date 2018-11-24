0d83e2c2c8ad2f37b606babd7750f96ede9d1186
codeking456 <sm544735@gmail.com>
2018-11-24 22:56:20 +0530

    Update Hamiltonian cycle.cpp

--- Hamiltonian cycle.cpp
+++ Hamiltonian cycle.cpp
@@ -1,66 +1,72 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-bool is_safe(bool v1[100][100],ll v, vector<ll> path, ll pos)
+ll f = 0;
+void print(vector<ll> &path)
 {
-	if (v1[path[pos - 1]][v] !=1)
-		return false;
-	for (ll i = 0; i < pos; i++)
-	{
-		if (path[i] == v)
-			return false;
-	}
-	return true;
+	for (ll i = 0; i < path.size(); i++)
+	  cout << path[i] << ' ';
+	  cout << '\n';
+	f = 1;
 }
-bool hem_util(bool v1[100][100], ll v, ll pos,vector<ll> &path)
+void ham_path(vector<ll> v1[], vector<bool> &visited, vector<ll> &path, ll s, ll v,bool g[100][100])
 {
-	if (pos == v)
+	
+	if (path.size() == v)
 	{
-		if (v1[path[pos - 1]][path[0]] == 1)
-			return true;
-		else
-			return false;
-	}
-	for (ll i = 2; i < v+1; i++)
-	{
-		if (is_safe(v1, i, path, pos))
+		if (g[path[path.size() - 1]][path[0]]==1)
 		{
-			path[pos] = i;
-			if (hem_util(v1, v, pos + 1, path) == true)
-				return true;
+			
+			print(path);
+			return;
+		}
 
-			path[pos] = -1;
+	}
+	for (auto it = v1[s].begin(); it != v1[s].end(); it++)
+	{
+		if (!visited[*it])
+		{
+			visited[*it] = true;
+			path.push_back(*it);
+			ham_path(v1, visited, path, *it, v,g);
+			visited[*it] = false;
+			path.pop_back();
+
 		}
 	}
-	return false;
 }
-void hem_cycle(bool v1[100][100], ll v)
+void ham_cycle(vector<ll> v1[], bool g[100][100], ll v)
 {
-	vector<ll> path(v+1,-1);
-	path[0] = 1;
-	if (hem_util(v1, v, 1, path) == false)
-	{
-		cout << "NO";
-		return;
-	}
-	else
-	{
-		for (ll i = 0; i < v; i++)
-			cout << path[i] << ' ';
-	}
+	vector<bool> visited(v + 2, false);
+	vector<ll> path;
+	path.push_back(1);
+	visited[1] = true;
+	ham_path(v1, visited, path, 1, v, g);
+	if (f == 0)
+		cout << "NO" << '\n';
 }
 int main()
 {
-	ll v, e, a, b;
-	cin >> v >> e;
-	bool v1[100][100];
-	for (ll i = 0; i <e; i++)
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll t;
+	cin >> t;
+	while (t--)
 	{
-		cin >> a >> b;
-		v1[a][b] = 1;
-		v1[b][a] = 1;
-
+		f = 0;
+		ll v, e, a, b;
+		bool g[100][100];
+		memset(g, 0, 10000);
+		vector<ll> v1[100];
+		cin >> v >> e;
+		for (ll i = 0; i < e; i++)
+		{
+			cin >> a >> b;
+			v1[a].push_back(b);
+			v1[b].push_back(a);
+			g[a][b] = 1;
+			g[b][a] = 1;
+		}
+		ham_cycle(v1, g, v);
 	}
-	hem_cycle(v1, v);
-	cin >> v;
 }
